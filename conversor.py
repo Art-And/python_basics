593506987eb73b07bada40c8d2f6cd2a425f5366
Andres <hir.islas@outlook.com>
2021-06-26 00:26:50 -0500

    Agrega al conversor un mini menu para elegir moneda, esto no es buena practica ya que se repite codigo, pero lo arregalmos en el siguiente commit

--- conversor.py
+++ conversor.py
@@ -1,7 +1,37 @@
-pesos = input('Pesos Mexicanos: ')
-pesos =  float(pesos)
-valor_dolar = 20
-dolar = pesos / valor_dolar
-dolar = round(dolar, 2)
-dolar = str(dolar)
-print('Tienes $' + dolar + ' dolares')
+menu = """
+Bienvenido al conversor de monedas 😀
+
+1 - Pesos Mexicanos
+2 - Pesos argentinos
+3 - Pesos Colombianos
+
+Elige una opción: """
+
+opcion = input(menu)
+
+if opcion == '1':
+    pesos = input('Pesos Mexicanos: ')
+    pesos =  float(pesos)
+    valor_dolar = 20
+    dolar = pesos / valor_dolar
+    dolar = round(dolar, 2)
+    dolar = str(dolar)
+    print('Tienes $' + dolar + ' dolares')
+elif opcion == '2':
+    pesos = input('Pesos Argentinos: ')
+    pesos =  float(pesos)
+    valor_dolar = 95
+    dolar = pesos / valor_dolar
+    dolar = round(dolar, 2)
+    dolar = str(dolar)
+    print('Tienes $' + dolar + ' dolares')
+elif opcion == '3':
+    pesos = input('Pesos Colombianos: ')
+    pesos =  float(pesos)
+    valor_dolar = 3746
+    dolar = pesos / valor_dolar
+    dolar = round(dolar, 2)
+    dolar = str(dolar)
+    print('Tienes $' + dolar + ' dolares')
+else:
+    print('Elige una opcion correcta')
