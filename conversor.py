640baee8080df7e0b7daeabecfbf96973df7effc
Andres <hir.islas@outlook.com>
2021-06-26 23:05:18 -0500

    Agrega funcion para evitar la duplicidad de codigo

--- conversor.py
+++ conversor.py
@@ -9,29 +9,19 @@ Elige una opción: """
 
 opcion = input(menu)
 
+def conversor(string_tipo_pesos, valor_dolar):
+    pesos = input('Convertir pesos ' + string_tipo_pesos + ': ')
+    pesos =  float(pesos)
+    dolar = pesos / valor_dolar
+    dolar = round(dolar, 2)
+    dolar = str(dolar)
+    print('Tienes $' + dolar + ' dolares')
+
 if opcion == '1':
-    pesos = input('Pesos Mexicanos: ')
-    pesos =  float(pesos)
-    valor_dolar = 20
-    dolar = pesos / valor_dolar
-    dolar = round(dolar, 2)
-    dolar = str(dolar)
-    print('Tienes $' + dolar + ' dolares')
+    conversor('mexicanos', 20)
 elif opcion == '2':
-    pesos = input('Pesos Argentinos: ')
-    pesos =  float(pesos)
-    valor_dolar = 95
-    dolar = pesos / valor_dolar
-    dolar = round(dolar, 2)
-    dolar = str(dolar)
-    print('Tienes $' + dolar + ' dolares')
+    conversor('argentinos', 95)
 elif opcion == '3':
-    pesos = input('Pesos Colombianos: ')
-    pesos =  float(pesos)
-    valor_dolar = 3746
-    dolar = pesos / valor_dolar
-    dolar = round(dolar, 2)
-    dolar = str(dolar)
-    print('Tienes $' + dolar + ' dolares')
+    conversor('colombianos', 3795)
 else:
     print('Elige una opcion correcta')
